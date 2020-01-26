d23d2ae11360686abb4e9a6edf808005dfdc1e2f
Thibault Walterspieler <thibaultwalterspieler@gmail.com>
2020-01-26 22:13:52 +0100

    feat : add author meta && merge js

--- assets/js/cli.js
+++ assets/js/cli.js
@@ -1,3 +1,7 @@
+/**
+ * @author Thibault Walterspieler <thibault@walterspieler.com>
+ */
+
 window.onload = (event) => {
     console.log([
         `                            `,
@@ -154,11 +158,6 @@ window.onload = (event) => {
 
 
     // Input possibilities
-
-
-
-
-
     cliInput.addEventListener('keypress', function (e) {
 
         if (e.key === 'Enter') {
@@ -167,7 +166,9 @@ window.onload = (event) => {
                 'help': () => {
                     injectCliResult(
                         `shutdown    : close down the system </br>
-                         disable --c : disable current replicant
+                         disable --c : disable current replicant</br>
+                         sudo rm -rf * : s delete recursively all files without any confirmation</br>
+                         More commands in cli.js
                         `, input)
                 },
                 'shutdown': () => {
@@ -185,8 +186,23 @@ window.onload = (event) => {
                         Error : Unable to reach rep N6MAA10816
                         `, input)
                 },
+                'awwwards': () => {
+                    injectCliResult(
+                        `Redirection to 217.160.0.180
+                        `, input)
+                    setTimeout(() => {
+                        window.open('https://bruno-simon.com', '_blank');
+                    }, 1500);
+                },
+                'sudo rm -rf *': () => {
+                    injectCliResult(
+                        `エラ$_error エラ$_error エラ$_e_1rror エラ$_error
+                        `, input)
+                    replicantScene.rotation.y = 400
+                },
+
                 'default': () => {
-                    injectCliResult('command not found: ' + input, input)
+                    injectCliResult(`command not found: Type help for commands list` + input, input)
                 }
             };
 
@@ -198,6 +214,84 @@ window.onload = (event) => {
         }
     })
 
+    setTimeout(() => {
+        const sceneContainer = document.querySelector('.replicant3d-wrapper')
+        const boundingClientRect = sceneContainer.getBoundingClientRect()
+        const sceneWidth = boundingClientRect.width
+        const sceneHeight = boundingClientRect.height
 
 
+        let scene = new THREE.Scene()
+        let camera = new THREE.PerspectiveCamera(75, sceneWidth / sceneHeight, 0.1, 1000)
+        let renderer = new THREE.WebGLRenderer({
+            antialias: true
+        })
+        renderer.setSize(sceneWidth, sceneHeight)
+        sceneContainer.appendChild(renderer.domElement)
+
+        // Set scene backgorund
+        scene.background = new THREE.Color(0x252525)
+        // Set light
+        let light = new THREE.AmbientLight(0x404040)
+        // Add light on scene 
+        scene.add(light)
+
+        camera.position.z = 5
+
+        let loader = new THREE.GLTFLoader()
+        let rep1
+        loader.load('./assets/models_3d/replicant.glb', function (gltf) {
+            replicantScene = gltf.scene
+            const box = new THREE.Box3().setFromObject(replicantScene)
+            const center = box.getCenter(new THREE.Vector3())
+            // Align scene
+            replicantScene.position.x += (gltf.scene.position.x - center.x)
+            replicantScene.position.y += (gltf.scene.position.y - center.y)
+            replicantScene.position.z += (gltf.scene.position.z - center.z)
+
+            body = replicantScene.children[0]
+            hand = replicantScene.children[1]
+
+
+            body.material.wireframe = true
+            hand.visible = true
+            body.material.wireframeLinewidth = .000001
+            hand.material.wireframe = true
+            hand.material.wireframeLinewidth = .000001
+            let light = new THREE.PointLight(0xecbb19)
+            light.position.set(50, 50, 50)
+            // body.geometry.center() 
+            //  hand.geometry.center() 
+            //  hand.material.color = new THREE.Color(0xecbb19) 
+            // let rep = new THREE.Mesh(rep1, materialRep) 
+            replicantScene.scale.set(.8, .8, .8)
+            // replicantScene.geometry.center() 
+            // body.geometry.center() 
+            // body.geometry.center() 
+            scene.add(replicantScene, light)
+
+            function animate() {
+                requestAnimationFrame(animate)
+                // Replicatant rotation 
+                // replicantScene.rotation.y += .01
+                // replicantScene.rotation.y += rotationSpeed
+                // Glitch hand 
+                setInterval(() => {
+                    hand.visible = false
+                    setInterval(() => {
+                        hand.visible = true
+                    }, 2000)
+                }, 1000)
+                //  hand.rotation.y += 0.01 
+                renderer.render(scene, camera)
+            }
+            animate()
+
+        }, undefined, function (error) {
+
+            console.error(error)
+
+        })
+
+    }, 3100)
 }
